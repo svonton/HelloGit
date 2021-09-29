785a29bde31a01ab08fc5c72a190883317723866
svonton <56838368+svonton@users.noreply.github.com>
2021-09-29 20:21:46 +0300

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,10 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main (String[] args){
+        String bot_name = "Aboba";
+        String birth_year = "2021";
+        System.out.println("Hello! My name is "+ bot_name +"\n" +
+                "I was created in "+birth_year+".\n");
+    }
+}
