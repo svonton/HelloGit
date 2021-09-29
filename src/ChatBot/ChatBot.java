3ddd65b26b55fdd4592a6174860fe54484f8c8ee
svonton <56838368+svonton@users.noreply.github.com>
2021-09-29 20:35:18 +0300

    ChatBot 3-rd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -7,7 +7,7 @@ public class ChatBot {
         String birth_year = "2021";
 
         System.out.println("Hello! My name is "+ bot_name +"\n" +
-                "I was created in "+birth_year+".\n");
+                "I was created in "+birth_year+".");
 
         System.out.println("Please, remind me your name.");
 
@@ -15,5 +15,16 @@ public class ChatBot {
         String user_name = scanner.nextLine();
 
         System.out.println("What a great name you have, "+user_name+"!");
+
+        System.out.println("Let me guess your age.\n" +
+                "Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is "+age+"; that's a good time to start programming!");
     }
 }
