45e9bb4730ff4a17262033608773bbe9782aaa1b
svonton <56838368+svonton@users.noreply.github.com>
2021-10-22 16:59:50 +0300

    2nd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,13 +1,20 @@
 package CoffeeMachine;
+import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args){
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!\n");
+        Scanner scanner = new Scanner(System.in);
+
+        Integer water_cost = 200;
+        Integer milk_cost = 50;
+        Integer beans_cost = 15;
+
+        System.out.println("Write how many cups of coffee you will need:");
+        Integer amount_of_cups = scanner.nextInt();
+
+        System.out.println("For "+amount_of_cups+" cups of coffee you will need:\n"
+                +amount_of_cups*water_cost+" ml of water\n"
+                +amount_of_cups*milk_cost+" ml of milk\n"
+                +amount_of_cups*beans_cost+" g of coffee beans");
     }
 }
